2029db987e98910e138c0fc06ade26b694848f88
bri-yan <bri.yan@outlook.com>
2023-08-28 12:21:52 -0700

    organized components

--- src/App.js
+++ src/App.js
@@ -14,19 +14,23 @@
  */
 import { Suspense, useMemo, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, useScroll, ScrollControls, Environment, Merged, Text, Text3D, MeshReflectorMaterial, Plane, useTexture } from '@react-three/drei'
+import { useGLTF, useScroll, ScrollControls, Environment, Merged, MeshReflectorMaterial } from '@react-three/drei'
 import About from "./cabins/About"
 import Default from './cabins/Default'
 
 function Train() {
+  // setup
   const mounted = useRef(false)
   const ref = useRef()
   const scroll = useScroll()
   const [cabin, seat] = useGLTF(['/cabin-transformed.glb', '/seat-transformed.glb'])
   const meshes = useMemo(() => ({ Cabin: cabin.nodes.cabin_1, Seat: seat.nodes.seat }), [cabin, seat])
-  const popcat = useTexture("./popcat.png")
+  
+  // train details
   const start = 8
   const spacing = 26
+
+  // scroll function
   useFrame(() => {
     if (!mounted.current) {
       if (cabin && seat) {
@@ -38,6 +42,7 @@ function Train() {
       ref.current.position.x = -8
     }
   })
+
   // Merged creates THREE.InstancedMeshes out of the meshes you feed it
   // All in all we end up with just 5 draw-calls for the entire scene
   return (
@@ -54,56 +59,6 @@ function Train() {
     </Merged>
   )
 }
-
-const Quarter = ({ models, color, ...props }) => (
-  <group {...props}>
-    <models.Seat color={color} position={[-0.35, 0, 0.7]} />
-    <models.Seat color={color} position={[0.35, 0, 0.7]} />
-    <models.Seat color={color} position={[-0.35, 0, -0.7]} rotation={[0, Math.PI, 0]} />
-    <models.Seat color={color} position={[0.35, 0, -0.7]} rotation={[0, Math.PI, 0]} />
-  </group>
-)
-
-const Row = ({ models, color, ...props }) => (
-  <group {...props}>
-    <Quarter models={models} color={color} position={[-1.2, -0.45, 9.75]} />
-    <Quarter models={models} color={color} position={[1.2, -0.45, 9.75]} />
-  </group>
-)
-
-const Blurb = ({header, body, texture, ...props }) => (
-  <group {...props}>
-    <Text3D
-      font="./Lato_Regular.json" size={1.5} height={0.12} position={[3.25, 1.8, -7]} rotation={[-Math.PI / 2, 0, 0]}
-    >
-      {header}
-    </Text3D>
-    <Text font="./Lato-Regular.ttf" fontSize={0.75} color="#ffffff" position={[3.15, 1.92, -5.75]} rotation={[-Math.PI / 2, 0, 0]} maxWidth={15} anchorX='left' anchorY='top' >
-      {body}
-    </Text>
-    <mesh position={[3.15, 2.5, -5.75]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry args={[10, 10]} />
-      <meshStandardMaterial map={texture} transparent repeat={[2,1]} />
-    </mesh>
-  </group>
-)
-
-const Cabin = ({ models, color = 'white', seatColor = 'white', header, body, texture, ...props }) => (
-  <group {...props}>
-    <Blurb header={header} body={body} texture={texture} />
-    <models.Cabin color={color} />
-    <Row models={models} color={seatColor} />
-    <Row models={models} color={seatColor} position={[0, 0, -1.9]} />
-    <Row models={models} color={seatColor} position={[0, 0, -6.6]} />
-    <Row models={models} color={seatColor} position={[0, 0, -8.5]} />
-    <Row models={models} color={seatColor} position={[0, 0, -11]} />
-    <Row models={models} color={seatColor} position={[0, 0, -12.9]} />
-    <Row models={models} color={seatColor} position={[0, 0, -17.6]} />
-    <Row models={models} color={seatColor} position={[0, 0, -19.5]} />
-  </group>
-)
-
-
 
 export default function App() {
   return (
