528ddfd4971fdd804a38aa16b1a8ceb0515cb3dd
bri-yan <bri.yan@outlook.com>
2026-02-14 18:47:07 -0800

    remove lights

--- src/components/TorusScene.jsx
+++ src/components/TorusScene.jsx
@@ -17,9 +17,9 @@ export function TorusScene() {
 
   return (
     <>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
-      <pointLight position={[-10, -10, -5]} intensity={0.5} color="#ff00ff" />
+      <ambientLight intensity={2} />
+      {/* <directionalLight position={[10, 10, 5]} intensity={1} />
+      <pointLight position={[-10, -10, -5]} intensity={0.5} color="#ff00ff" /> */}
 
       <mesh ref={torusRef}>
         <torusGeometry args={[1, 0.4, 32, 100]} />
