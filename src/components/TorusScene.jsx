9eb87b81bf6a9daea5325992b841d0fd47d61729
brian.yan <brian.yan@bytedance.com>
2026-02-18 23:23:46 -0800

    edited rotation speed

--- src/components/TorusScene.jsx
+++ src/components/TorusScene.jsx
@@ -1,8 +1,8 @@
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 
-const ROTATION_SPEED_X = 0.3;
-const ROTATION_SPEED_Y = 0.5;
+const ROTATION_SPEED_X = 0.2;
+const ROTATION_SPEED_Y = 0.6;
 
 export function TorusScene() {
   const meshRef = useRef();
